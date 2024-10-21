5f9933fd6ac582d6709abb133c7089e50147b68c
Alex Oliva <alexoliva87464@gmail.com>
2024-10-20 21:41:03 -0300

    feat: add validations to 'update' products service

--- src/products/entities/product.entity.ts
+++ src/products/entities/product.entity.ts
@@ -1,4 +1,10 @@
-import { BeforeInsert, Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+import {
+  BeforeInsert,
+  BeforeUpdate,
+  Column,
+  Entity,
+  PrimaryGeneratedColumn,
+} from 'typeorm';
 
 @Entity()
 export class Product {
@@ -35,12 +41,20 @@ export class Product {
   // images
 
   @BeforeInsert()
-  checkSlug() {
+  checkSlugInsert() {
     if (!this.slug) {
       this.slug = this.title;
     }
 
-    this.slug = this.title
+    this.slug = this.slug
+      .toLowerCase()
+      .replaceAll(' ', '_')
+      .replaceAll("'", '');
+  }
+
+  @BeforeUpdate()
+  checkSlugUpdate() {
+    this.slug = this.slug
       .toLowerCase()
       .replaceAll(' ', '_')
       .replaceAll("'", '');
