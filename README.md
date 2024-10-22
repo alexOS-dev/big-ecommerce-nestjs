4e96dcf97d0b3b71a9a4f54b9c3bfb8307b80acd
Alex Oliva <alexoliva87464@gmail.com>
2024-10-21 22:58:18 -0300

    feat: add step to run the repository & implement 'create' products functionality in seed service

--- README.md
+++ README.md
@@ -5,22 +5,27 @@
 # Installation
 
 1. Install dependencies
+
 ```bash
 pnpm install
 ```
 
 2. Copy `.env.template` to `.env` and fill the variables
+
 ```bash
 cp .env.template .env
 ```
 
 3. Run the database
+
 ```bash
 docker-compose up -d
 ```
 
-4. Run the application
+4. Seed the database with initial data, open: `http://localhost:3000/api/seed`
+
+5. Run the application
+
 ```bash
 pnpm start:dev
 ```
-
--- src/seed/seed.service.ts
+++ src/seed/seed.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@nestjs/common';
 import { ProductsService } from 'src/products/products.service';
+import { initialData } from './data/seed-data';
 
 @Injectable()
 export class SeedService {
@@ -12,6 +13,17 @@ export class SeedService {
 
   private async insertNewProduct() {
     await this.productsService.deleteAllProducts();
+
+    const seedProducts = initialData.products;
+
+    const insertPromises = [];
+
+    seedProducts.forEach((product) => {
+      insertPromises.push(this.productsService.create(product));
+    });
+
+    await Promise.all(insertPromises);
+
     return true;
   }
 }
