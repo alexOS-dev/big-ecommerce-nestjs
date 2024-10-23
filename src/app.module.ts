7bbc4b7d1338b8cca3ecf174a4c4447ec5086ed1
Alex Oliva <alexoliva87464@gmail.com>
2024-10-23 03:09:18 -0300

    feat: add serve static module for images in public folder

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,12 @@
+import { join } from 'path';
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { ProductsModule } from './products/products.module';
+import { ServeStaticModule } from '@nestjs/serve-static';
 import { CommonModule } from './common/common.module';
-import { SeedModule } from './seed/seed.module';
+import { ProductsModule } from './products/products.module';
 import { FilesModule } from './files/files.module';
+import { SeedModule } from './seed/seed.module';
 
 @Module({
   imports: [
@@ -19,6 +21,9 @@ import { FilesModule } from './files/files.module';
       autoLoadEntities: true,
       // Synchronize the schema with the entities
       synchronize: true, // ! disable this for production
+    }),
+    ServeStaticModule.forRoot({
+      rootPath: join(__dirname, '..', 'public'),
     }),
     ProductsModule,
     CommonModule,
