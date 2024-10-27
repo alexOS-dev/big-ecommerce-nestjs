7f7208770b52d79031bd7118f40b5864c26a699b
Alex Oliva <alexoliva87464@gmail.com>
2024-10-27 08:14:04 -0300

    feat: add relation between user & products to track what user created each product

--- src/products/entities/product.entity.ts
+++ src/products/entities/product.entity.ts
@@ -3,10 +3,12 @@ import {
   BeforeUpdate,
   Column,
   Entity,
+  ManyToOne,
   OneToMany,
   PrimaryGeneratedColumn,
 } from 'typeorm';
 import { ProductImage } from '.';
+import { User } from 'src/auth/entities/user.entity';
 
 @Entity()
 export class Product {
@@ -49,6 +51,9 @@ export class Product {
     eager: true,
   })
   images?: ProductImage[];
+
+  @ManyToOne(() => User, (user) => user.product, { eager: true })
+  user: User;
 
   @BeforeInsert()
   checkSlugInsert() {
