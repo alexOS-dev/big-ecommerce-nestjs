5b09d61f3e7dc58a337a631879ac522d9b912a05
Alex Oliva <alexoliva87464@gmail.com>
2024-10-27 17:55:32 -0300

    feat: add userId to created products in seed

--- src/seed/data/seed-data.ts
+++ src/seed/data/seed-data.ts
@@ -1,3 +1,5 @@
+import * as bcrypt from 'bcrypt';
+
 interface SeedProduct {
   description: string;
   images: string[];
@@ -11,14 +13,43 @@ interface SeedProduct {
   gender: 'men' | 'women' | 'kid' | 'unisex';
 }
 
+interface SeedUser {
+  email: string;
+  fullName: string;
+  password: string;
+  roles: string[];
+}
+
 type ValidSizes = 'XS' | 'S' | 'M' | 'L' | 'XL' | 'XXL' | 'XXXL';
 type ValidTypes = 'shirts' | 'pants' | 'hoodies' | 'hats';
 
 interface SeedData {
+  users: SeedUser[];
   products: SeedProduct[];
 }
 
 export const initialData: SeedData = {
+  users: [
+    {
+      fullName: 'Alex Oliva',
+      email: 'alex@google.com',
+      password: bcrypt.hashSync('Abc123!', 10),
+      roles: ['user', 'admin'],
+    },
+    {
+      fullName: 'Juan',
+      email: 'juan@google.com',
+      password: bcrypt.hashSync('Abc123!', 10),
+      roles: ['user'],
+    },
+    {
+      fullName: 'Marcos',
+      email: 'marcos@google.com',
+      password: bcrypt.hashSync('Abc123!', 10),
+      roles: ['admin'],
+    },
+  ],
+
   products: [
     {
       description:
