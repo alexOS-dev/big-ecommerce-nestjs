1a2c95daf14e20c4ff34ab9164f97f00d0bc4078
Alex Oliva <alexoliva87464@gmail.com>
2024-10-28 19:08:18 -0300

    feat: add ApiProperty to entities & dtos

--- src/products/dto/create-product.dto.ts
+++ src/products/dto/create-product.dto.ts
@@ -1,3 +1,4 @@
+import { ApiProperty } from '@nestjs/swagger';
 import {
   IsArray,
   IsIn,
@@ -10,40 +11,85 @@ import {
 } from 'class-validator';
 
 export class CreateProductDto {
+  @ApiProperty({
+    default: 'Your Product Title (unique)',
+    nullable: false,
+    minLength: 1,
+    uniqueItems: true,
+  })
   @IsString()
   @MinLength(1)
   title: string;
 
+  @ApiProperty({
+    default: 0,
+    minimum: 0,
+    nullable: true,
+  })
   @IsNumber()
   @IsPositive()
   @IsOptional()
   price?: number;
 
+  @ApiProperty({
+    default: 'Product description',
+    nullable: true,
+  })
   @IsString()
   @IsOptional()
   description?: string;
 
+  @ApiProperty({
+    description:
+      'Product slug for SEO & URL/routing. If not provided, title will be used to generate it',
+    example: 'product_slug_1',
+    nullable: true,
+  })
   @IsString()
   @IsOptional()
   slug?: string;
 
+  @ApiProperty({
+    default: 0,
+    minimum: 0,
+    nullable: true,
+  })
   @IsInt()
   @IsPositive()
   @IsOptional()
   stock?: number;
 
+  @ApiProperty({
+    type: [String],
+    isArray: true,
+  })
   @IsString({ each: true })
   @IsArray()
   sizes: string[];
 
+  @ApiProperty({
+    name: 'Gender',
+    enum: ['men', 'women', 'unisex', 'kid'],
+  })
   @IsIn(['men', 'women', 'unisex', 'kid'])
   gender: string;
 
+  @ApiProperty({
+    type: [String],
+    isArray: true,
+    nullable: true,
+  })
   @IsString({ each: true })
   @IsArray()
   @IsOptional()
   tags: string[];
 
+  @ApiProperty({
+    type: [String],
+    isArray: true,
+    nullable: true,
+    example: ['http://image1.jpg', 'http://image2.jpg'],
+  })
   @IsString({ each: true })
   @IsArray()
   @IsOptional()
--- src/products/entities/product.entity.ts
+++ src/products/entities/product.entity.ts
@@ -9,43 +9,89 @@ import {
 } from 'typeorm';
 import { ProductImage } from '.';
 import { User } from 'src/auth/entities/user.entity';
+import { ApiProperty } from '@nestjs/swagger';
 
 @Entity()
 export class Product {
+  @ApiProperty({
+    example: '087100cf-b431-48da-a237-bc4900a6e13b',
+    description: 'Product ID',
+    uniqueItems: true,
+  })
   @PrimaryGeneratedColumn('uuid')
   id: string;
 
+  @ApiProperty({
+    example: 'Lenovo ThinkPad X1 Carbon Gen 11 (14” Intel) Laptop',
+    description: 'Product Title',
+    uniqueItems: true,
+  })
   @Column('text', {
     unique: true,
   })
   title: string;
 
+  @ApiProperty({ example: 0, description: 'Product Price' })
   @Column('float', { default: 0 })
   price: number;
 
+  @ApiProperty({
+    example:
+      'Ipsum proident exercitation nisi voluptate et eiusmod magna adipisicing aliquip duis ea id nostrud est.',
+    description: 'Product Description',
+    default: null,
+  })
   @Column({
     type: 'text',
     nullable: true,
   })
   description: string;
 
+  @ApiProperty({
+    example: 'lenovo_thinkpad_x1_carbon_gen_11_(14”_intel)_laptop',
+    description: 'Product Slug to generate the URL',
+    uniqueItems: true,
+  })
   @Column('text', { unique: true })
   slug: string;
 
+  @ApiProperty({
+    example: 10,
+    description: 'Product Stock',
+    default: 0,
+    minimum: 0,
+  })
   @Column('int', { default: 0 })
   stock: number;
 
+  @ApiProperty({
+    example: ['S', 'M', 'XL', 'XXL'],
+    description: 'Product Sizes',
+    isArray: true,
+    type: [String],
+  })
   @Column('text', { array: true })
   sizes: string[];
 
+  @ApiProperty({
+    description: 'Product Stock',
+    example: 'men',
+  })
   @Column('text')
   gender: string;
 
   // tags
+  @ApiProperty({
+    description: 'Product Tags for SEO',
+    example: ['laptop', 'pc', 'computer', 'lenovo', 'tech', 'technology'],
+    isArray: true,
+    type: [String],
+  })
   @Column('text', { array: true, default: [] })
   tags: string[];
 
   // images
+  @ApiProperty()
   @OneToMany(() => ProductImage, (productImage) => productImage.product, {
     cascade: true,
     eager: true,
