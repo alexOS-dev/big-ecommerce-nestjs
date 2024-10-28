6c8fe31822576cd9e907891ae1e6c0b6001cfa7b
Alex Oliva <alexoliva87464@gmail.com>
2024-10-28 19:11:39 -0300

    feat: add swagger decorators for the seed route

--- src/seed/seed.controller.ts
+++ src/seed/seed.controller.ts
@@ -1,10 +1,16 @@
 import { Controller, Get } from '@nestjs/common';
+import { ApiOkResponse, ApiTags } from '@nestjs/swagger';
 import { SeedService } from './seed.service';
 
+@ApiTags('Seed')
 @Controller('seed')
 export class SeedController {
   constructor(private readonly seedService: SeedService) {}
 
+  @ApiOkResponse({
+    description: 'Seed executed',
+    schema: { type: 'string', example: '* Seed executed *' },
+  })
   @Get()
   // @Auth(ValidRoles.admin)
   executeSeed() {
