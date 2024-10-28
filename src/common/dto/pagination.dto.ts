1bdde36eab9c2349e42861b6cab6626c08516e0b
Alex Oliva <alexoliva87464@gmail.com>
2024-10-28 19:13:12 -0300

    feat: add @ApiProperty to pagination dto

--- src/common/dto/pagination.dto.ts
+++ src/common/dto/pagination.dto.ts
@@ -1,7 +1,13 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { Type } from 'class-transformer';
 import { IsNumber, IsOptional, IsPositive, Min } from 'class-validator';
 
 export class PaginationDto {
+  @ApiProperty({
+    default: 10,
+    description: 'How many rows do you need',
+    minimum: 1,
+  })
   @IsOptional()
   @IsPositive()
   @IsNumber()
@@ -9,6 +15,11 @@ export class PaginationDto {
   @Type(() => Number)
   limit?: number;
 
+  @ApiProperty({
+    default: 0,
+    description: 'How many rows do you want to skip',
+    minimum: 1,
+  })
   @IsOptional()
   @IsNumber()
   @Min(0)
