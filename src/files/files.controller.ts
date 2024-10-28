d8372b2fc433baaa136a611a1fef45d735727657
Alex Oliva <alexoliva87464@gmail.com>
2024-10-28 19:08:41 -0300

    feat: add swagger decorators to each route in files controller

--- src/files/files.controller.ts
+++ src/files/files.controller.ts
@@ -9,12 +9,22 @@ import {
   UploadedFile,
   UseInterceptors,
 } from '@nestjs/common';
+import {
+  ApiBadRequestResponse,
+  ApiBody,
+  ApiConsumes,
+  ApiOkResponse,
+  ApiTags,
+} from '@nestjs/swagger';
 import { ConfigService } from '@nestjs/config';
 import { FileInterceptor } from '@nestjs/platform-express';
+
 import { diskStorage } from 'multer';
+
 import { FilesService } from './files.service';
 import { fileFilter, fileNamer } from './helpers';
 
+@ApiTags('Files - Get & Upload')
 @Controller('files')
 export class FilesController {
   constructor(
@@ -23,6 +33,8 @@ export class FilesController {
   ) {}
 
   @Get('product/:imageName')
+  @ApiOkResponse({ description: 'Image found and returned successfully' })
+  @ApiBadRequestResponse({ description: 'Image not found' })
   findProductImage(
     @Res() res: Response,
     @Param('imageName') imageName: string,
@@ -36,13 +48,38 @@ export class FilesController {
   @UseInterceptors(
     FileInterceptor('file', {
       fileFilter: fileFilter,
-      // limits: {}, // ? optional to set limits
       storage: diskStorage({
         destination: './static/products',
         filename: fileNamer,
       }),
     }),
   )
+  @ApiConsumes('multipart/form-data')
+  @ApiBody({
+    description: 'Image file to upload',
+    schema: {
+      type: 'object',
+      properties: {
+        file: {
+          type: 'string',
+          format: 'binary',
+        },
+      },
+    },
+  })
+  @ApiOkResponse({
+    description: 'Image uploaded successfully',
+    schema: {
+      type: 'object',
+      properties: {
+        secureUrl: {
+          type: 'string',
+          example: 'http://localhost:3000/files/product/imagen.jpg',
+        },
+      },
+    },
+  })
+  @ApiBadRequestResponse({ description: 'Make sure the file is an image' })
   uploadProductImage(@UploadedFile() file: Express.Multer.File) {
     if (!file) throw new BadRequestException('Make sure the file is an image');
 
